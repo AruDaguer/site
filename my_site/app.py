80877ebd6cc1c084595a3c4ca4677423acb2cff7
laura daguer <estudante1@debian.spo.ifsp.edu.br>
2024-06-19 15:10:38 -0300

    formulários e imagens

--- my_site/app.py
+++ my_site/app.py
@@ -1,34 +1,44 @@
-import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 lista_produtos = [
-        {"nome": "Coca-Cola", "descricao": "Bom"},
-        {"nome": "Doritos", "descricao": "Suja Mão"},
-        {"nome": "Chocolate", "descricao": "Bom"}
+        {"nome": "Coca-Cola", "descricao": "Bom", "preco": 5.00, "imagem": "https://api.freelogodesign.org/assets/blog/thumb/f4dae7732213491da3952f853c48f6dc_1176x840.jpg"},
+        {"nome": "Doritos", "descricao": "Suja Mão", "preco": 7.50, "imagem": "https://1.bp.blogspot.com/-berlgYpJS50/Wvr5Q44W7bI/AAAAAAABqnA/p0J4dlS5urgIEUl0AdW_fz50B7hRBduTQCLcBGAs/s1600/doritos%2B2.jpg"},
+        {"nome": "Chocolate", "descricao": "Bom", "preco": 4.75, "imagem": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRUD5sBMqGcsSFV-RFdy8KxLsscLWdEWStXA&s"}
 ]
 
 @app.route("/")
 def home():
-    return "<h1>Home</h1>"
-
-@app.route("/contato")
-def contato():
-    return "<h1>Contato</h1>"
+    return render_template("home.html")
 
 @app.route("/produtos")
 def produtos():
-    
-    return render_template('produtos.html', produtos=lista_produtos)
+    return render_template("produtos.html", produtos=lista_produtos)
+
 
 @app.route("/produtos/<nome>")
 def produto(nome):
     for produto in lista_produtos:
         if produto["nome"] == nome:
-            return f"{produto['nome']},{produto['descricao']}"
-    return "Produto não encontrado"
+            return render_template("produto.html", produto=produto)
+        
+    return "Erro: Produto não encontrado"
+    
+#GET
+@app.route("/produtos/cadastrar")
+def cadastro_produto():
+    return render_template("cadastro-produto.html")
 
-if __name__=="__main__":
-    app.run(host=os.getenv('IP', '0.0.0.0'), 
-            port=int(os.getenv('PORT', 4444)))
+@app.route("/produtos", methods=['POST'])
+def salvar_produto():
+    nome = request.form['nome']
+    descricao = request.form['descricao']
+    preco = request.form['preco']
+    imagem = request.form['imagem']
+
+    produto = {"nome": nome, "descricao": descricao, "preco" : preco, "imagem": imagem }
+    lista_produtos.append(produto)
+
+    return redirect( url_for("produtos"))
+
